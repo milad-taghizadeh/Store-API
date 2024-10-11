e7e21751001fdd5ecbfff1de8282c102f513cc5c
Milad Taghizadeh <miladtaghizadeh1382@outlook.com>
2024-10-11 22:51:24 +0330

    feat: repository for product created

---
+++ src/database/migrations/20241011185124_test1/migration.sql
@@ -0,0 +1,97 @@
+-- CreateEnum
+CREATE TYPE "userStatus" AS ENUM ('NOT_VALIDATED', 'VALIDATED', 'DELETED', 'BANNED', 'TERMINATED');
+
+-- CreateEnum
+CREATE TYPE "discountMode" AS ENUM ('PERCENTAGE', 'REDUCE_PRICE');
+
+-- CreateEnum
+CREATE TYPE "productStatus" AS ENUM ('STOP_SELL', 'SOLD_OUT', 'FOR_SALE');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" UUID NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "email" TEXT,
+    "phone" TEXT NOT NULL,
+    "status" "userStatus" NOT NULL,
+    "discountId" INTEGER,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "otp" (
+    "id" SERIAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "isUsed" BOOLEAN NOT NULL DEFAULT true,
+    "expiresIn" TIMESTAMP(5) NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "product_name" TEXT NOT NULL,
+    "product_code" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
+    "final_price" TEXT NOT NULL,
+    "detail" TEXT[],
+    "color" TEXT[],
+    "images" TEXT NOT NULL,
+    "cover" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "categorty" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "productStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "userId" UUID NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Discount" (
+    "id" SERIAL NOT NULL,
+    "percentage" INTEGER,
+    "reduce_price" INTEGER,
+    "discount_mode" "discountMode" NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "value" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Discount_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_phone_key" ON "user"("phone");
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_discountId_fkey" FOREIGN KEY ("discountId") REFERENCES "Discount"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/modules/product/product.repository.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from '@prisma/client';
+import { Repository } from 'src/common/interfaces/repository';
+import { PrismaService } from 'src/database/database.service';
+
+@Injectable()
+export class ProductRepository implements Repository<Product> {
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(
+    data: Omit<Product, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Product> {
+    return await this.prismaService.product.create({
+      data: {
+        ...data,
+      },
+    });
+  }
+
+  async update(id: number, data: Partial<Product>): Promise<Product> {
+    return await this.prismaService.product.update({
+      where: {
+        id,
+      },
+      data: {
+        ...data,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Product> {
+    return await this.prismaService.product.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findByProductCode (productCode : string): Promise<Product> {
+    return await this.prismaService.product.findUnique({
+        where :{
+            productCode
+        }
+    })
+  }
+
+//   FIXME: fix findmany in category find 
+  async findByCategory (category : string): Promise<Product> {
+    return await this.prismaService.product.findFirst({
+        where :{
+            category
+        }
+    })
+  }
+}
