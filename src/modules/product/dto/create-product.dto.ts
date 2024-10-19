3b2ce9c9cca2348810e0fec3bc7df0b4935d831f
Milad Taghizadeh <miladtaghizadeh1382@outlook.com>
2024-10-19 13:52:43 +0330

    feat: dto for create product created

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,1 +1,67 @@
-export class CreateProductDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { productStatus } from "@prisma/client";
+import { IsString, IsNotEmpty, Length, IsNumber } from "class-validator";
+
+export class CreateProductDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @Length(0, 50)
+    productName: string;  
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @Length(4, 10)
+    productCode: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    price: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    finalPrice: string;
+
+    @ApiProperty()
+    @IsString()
+    detail: string[];
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    color: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    images: string[];
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    cover: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    qty: number;
+    
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @Length(0, 100)
+    description: string;
+    
+    @ApiProperty({enum: productStatus})
+    @IsString()
+    @Length(0, 100)
+    status: productStatus;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    categoryId: number;
+}
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -2,32 +2,35 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('product')
+@ApiTags("Product")
 export class ProductController {
+
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
+  @Post('new')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
-  @Get()
+  @Get('get')
   findAll() {
     return this.productService.findAll();
   }
 
-  @Get(':id')
+  @Get('get/:id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('update/:id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
+  @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
   }
