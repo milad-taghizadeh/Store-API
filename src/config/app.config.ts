5903e76fcfe957a0e10e066c901896aa73313b5d
Milad Taghizadeh <miladtaghizadeh1382@outlook.com>
2024-10-20 19:47:15 +0330

    update: swagger config updated

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,10 +8,10 @@ export enum ConfigKeys {
 const AppConfig = registerAs(ConfigKeys.App, () => ({
   SERVER_PORT: 3000,
   SWAGGER_ENDPOINT: 'api-doc',
-  SWAGGER_TITLE: 'Makan-Backend',
-  SWAGGER_DESCRIPTION: 'Api endpoints of Makan App',
-  SWAGGER_VERSION: '1.0.0',
-  SWAGGER_ADD_TAG: 'TEST',
+  SWAGGER_TITLE: 'Store-API',
+  SWAGGER_DESCRIPTION: 'Api endpoints of a store application',
+  SWAGGER_VERSION: '2.0.0',
+  // SWAGGER_ADD_TAG: 'TEST',
 }));
 
 export const configurations = [AppConfig];
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -13,34 +13,34 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
-@ApiTags('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    return await this.userService.createUser(createUserDto);
-  }
+  // @Post()
+  // async createUser(@Body() createUserDto: CreateUserDto) {
+  //   return await this.userService.createUser(createUserDto);
+  // }
 
-  @Get()
-  async findAll() {
-    return await this.userService.findAll();
-  }
+  // @Get()
+  // async findAll() {
+  //   return await this.userService.findAll();
+  // }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.userService.findOne(id);
-  }
+  // @Get(':id')
+  // async findOne(@Param('id') id: string) {
+  //   return await this.userService.findOne(id);
+  // }
 
-  @Get('phone/:phone')
-  async findByPhone(@Param('phone') phone: string) {
-    return await this.userService.findByPhone(phone);
-  }
+  // @Get('phone/:phone')
+  // async findByPhone(@Param('phone') phone: string) {
+  //   return await this.userService.findByPhone(phone);
+  // }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return await this.userService.update(id, updateUserDto);
-  }
+  // @Patch(':id')
+  // async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return await this.userService.update(id, updateUserDto);
+  // }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
